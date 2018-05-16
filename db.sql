7a747f3273f2577334bf68238473995aaf920397
James Nodws <Nodws@users.noreply.github.com>
2018-05-16 10:34:38 -0600

    start at ID 100 for better urls

--- db.sql
+++ db.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `stubs` (
   `id` int(11) NOT NULL,
-  `uid` varchar(255) NOT NULL,
-  `hits` int(11) NOT NULL,
+  `uid` varchar(255) NOT NULL DEFAULT '',
+  `hits` int(11) NOT NULL DEFAULT '0',
   `url` text NOT NULL,
-  `del` varchar(255) NOT NULL
+  `del` varchar(255) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -12,4 +12,4 @@ ALTER TABLE `stubs`
 
 
 ALTER TABLE `stubs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
